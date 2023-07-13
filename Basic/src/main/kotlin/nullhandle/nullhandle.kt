fdb5e131fd6553a6a503735628352023cbe1bf83
JangDeukChun <85157729+JangDeukchun@users.noreply.github.com>
2023-07-13 15:11:45 +0900

    null

--- Basic/src/main/kotlin/nullhandle/nullhandle.kt
+++ Basic/src/main/kotlin/nullhandle/nullhandle.kt
@@ -8,7 +8,6 @@ fun main() {
 fun returnKim(areYouKim: Boolean = false): Kim? = if(areYouKim) {
     /**
      * returnKim 함수: 이 함수는 areYouKim이라는 Boolean 타입 매개변수를 받습니다. 기본값은 false
-     *
      * 반환 타입: Kim? (nullable Kim 객체를 반환)
      * 동작: 만약 areYouKim == true 이면 Kim("kim") 객체를 생성하여 반환합니다. 그렇지 않으면 null 반환합니다.
      */
